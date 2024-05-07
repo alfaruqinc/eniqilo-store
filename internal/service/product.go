0e4dabedcc7e4e0577c96e0ade406d1559904997
umar006 <umaralfaruq597@gmail.com>
2024-05-07 22:30:25 +0700

    feat: parse query params

--- internal/service/product.go
+++ internal/service/product.go
@@ -5,11 +5,17 @@ import (
 	"database/sql"
 	"eniqilo-store/internal/domain"
 	"eniqilo-store/internal/repository"
+	"fmt"
+	"reflect"
+	"slices"
+	"strings"
+
+	"github.com/google/uuid"
 )
 
 type ProductService interface {
 	CreateProduct(ctx context.Context, product domain.Product) domain.MessageErr
-	GetProducts(ctx context.Context, queryParams string) ([]domain.ProductResponse, domain.MessageErr)
+	GetProducts(ctx context.Context, queryParams domain.ProductQueryParams) ([]domain.ProductResponse, domain.MessageErr)
 	UpdateProductByID(ctx context.Context, product domain.Product) domain.MessageErr
 	DeleteProductByID(ctx context.Context, productId string) domain.MessageErr
 }
@@ -46,19 +52,96 @@ func (ps *productService) CreateProduct(ctx context.Context, product domain.Prod
 	return nil
 }
 
-func (ps *productService) GetProducts(ctx context.Context, queryParams string) ([]domain.ProductResponse, domain.MessageErr) {
-	tx, err := ps.db.Begin()
-	if err != nil {
-		return nil, domain.NewInternalServerError(err.Error())
-	}
-	defer tx.Rollback()
+func (ps *productService) GetProducts(ctx context.Context, queryParams domain.ProductQueryParams) ([]domain.ProductResponse, domain.MessageErr) {
+	var query string
+	var limitOffsetClause []string
+	var whereClause []string
+	var orderClause []string
+	var args []any
 
-	products, err := ps.productRepository.GetProducts(ctx, tx, queryParams)
-	if err != nil {
-		return nil, domain.NewInternalServerError(err.Error())
+	val := reflect.ValueOf(queryParams)
+	typ := val.Type()
+
+	for i := 0; i < val.NumField(); i++ {
+		key := strings.ToLower(typ.Field(i).Name)
+		value := val.Field(i).String()
+		argPos := len(args) + 1
+
+		if key == "limit" || key == "offset" {
+			if key == "limit" && len(value) < 1 {
+				value = "5"
+			}
+			if key == "offset" && len(value) < 1 {
+				value = "0"
+			}
+
+			limitOffsetClause = append(limitOffsetClause, fmt.Sprintf("%s $%d", key, argPos))
+			args = append(args, value)
+			continue
+		}
+
+		if len(value) < 1 {
+			continue
+		}
+
+		if key == "id" {
+			if _, err := uuid.Parse(value); err != nil {
+				continue
+			}
+		}
+
+		if key == "name" {
+			whereClause = append(whereClause, fmt.Sprintf("%s ILIKE $%d", key, argPos))
+			args = append(args, "%"+value+"%")
+			continue
+		}
+
+		if key == "isavailable" {
+			key = "is_available"
+		}
+
+		if key == "category" {
+			if !slices.Contains(domain.ProductCategory, value) {
+				continue
+			}
+		}
+
+		if key == "price" || key == "createdat" {
+			if value != "asc" && value != "desc" {
+				continue
+			}
+			if key == "createdat" {
+				key = "created_at"
+			}
+
+			orderClause = append(orderClause, fmt.Sprintf("%s %s", key, value))
+			continue
+		}
+
+		if key == "instock" {
+			key = "stock"
+			if value == "true" {
+				whereClause = append(whereClause, fmt.Sprintf("%s > 0", key))
+			} else if value == "false" {
+				whereClause = append(whereClause, fmt.Sprintf("%s < 1", key))
+			}
+
+			continue
+		}
+
+		whereClause = append(whereClause, fmt.Sprintf("%s = $%d", key, argPos))
+		args = append(args, value)
 	}
 
-	err = tx.Commit()
+	if len(whereClause) > 0 {
+		query += "\nWHERE " + strings.Join(whereClause, " AND ")
+	}
+	if len(orderClause) > 0 {
+		query += "\nORDER BY " + strings.Join(orderClause, ", ")
+	}
+	query += "\n" + strings.Join(limitOffsetClause, " ")
+
+	products, err := ps.productRepository.GetProducts(ctx, ps.db, query, args)
 	if err != nil {
 		return nil, domain.NewInternalServerError(err.Error())
 	}
