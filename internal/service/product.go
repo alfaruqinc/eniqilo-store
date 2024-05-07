102f4cb4a961edf7730fdbde870350b7e95a9fb1
umar006 <umaralfaruq597@gmail.com>
2024-05-07 13:47:13 +0700

    feat: update product service

--- internal/service/product.go
+++ internal/service/product.go
@@ -9,6 +9,7 @@ import (
 
 type ProductService interface {
 	CreateProduct(ctx context.Context, product domain.Product) domain.MessageErr
+	UpdateProduct(ctx context.Context, product domain.Product) domain.MessageErr
 }
 
 type productService struct {
@@ -31,6 +32,29 @@ func (ps *productService) CreateProduct(ctx context.Context, product domain.Prod
 	defer tx.Rollback()
 
 	err = ps.productRepository.CreateProduct(ctx, tx, product)
+	if err != nil {
+		return domain.NewInternalServerError(err.Error())
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return domain.NewInternalServerError(err.Error())
+	}
+
+	return nil
+}
+
+func (ps *productService) UpdateProduct(ctx context.Context, product domain.Product) domain.MessageErr {
+	tx, err := ps.db.Begin()
+	if err != nil {
+		return domain.NewInternalServerError(err.Error())
+	}
+	defer tx.Rollback()
+
+	err = ps.productRepository.UpdateProduct(ctx, tx, product)
+	if err != nil {
+		return domain.NewInternalServerError(err.Error())
+	}
 
 	err = tx.Commit()
 	if err != nil {
