1d442a2195abdac107ab94af7555588043e639ec
umar006 <umaralfaruq597@gmail.com>
2024-05-09 06:05:00 +0700

    feat: check if customer or product is exists

--- internal/service/product-checkout.go
+++ internal/service/product-checkout.go
@@ -5,6 +5,8 @@ import (
 	"database/sql"
 	"eniqilo-store/internal/domain"
 	"eniqilo-store/internal/repository"
+
+	"github.com/jackc/pgx/v5/pgconn"
 )
 
 type CheckoutService interface {
@@ -34,6 +36,16 @@ func (cs *checkoutService) CreateCheckout(ctx context.Context, body domain.Check
 
 	err = cs.checkoutRepository.CreateCheckout(ctx, tx, checkout, productCheckouts)
 	if err != nil {
+		if err, ok := err.(*pgconn.PgError); ok {
+			customerNotFound := err.Code == "23503" && err.ConstraintName == "fk_user_customer_id_checkouts"
+			productsNotFound := err.Code == "23503" && err.ConstraintName == "fk_product_id_product_checkouts"
+			if customerNotFound {
+				return domain.NewNotFoundError("customer is not found")
+			} else if productsNotFound {
+				return domain.NewNotFoundError("one of your products is not found")
+			}
+		}
+
 		return domain.NewInternalServerError(err.Error())
 	}
 
