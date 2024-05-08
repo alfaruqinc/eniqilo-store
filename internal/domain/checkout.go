f9990bbcebf3a73dc5f145665dfd7a5737c3ebb7
umar006 <umaralfaruq597@gmail.com>
2024-05-09 05:13:27 +0700

    feat: normalize checkouts table
    create product_checkouts to save product item

--- internal/domain/checkout.go
+++ internal/domain/checkout.go
@@ -10,21 +10,27 @@ type Checkout struct {
 	ID             string    `db:"id"`
 	CreatedAt      time.Time `db:"created_at"`
 	UserCustomerID string    `db:"user_customer_id"`
-	ProductID      string    `db:"product_id"`
 	Paid           int       `db:"paid"`
 	Change         int       `db:"change"`
 }
 
+type ProductCheckout struct {
+	ID         string `db:"id"`
+	ProductID  string `db:"product_id"`
+	Quantity   int    `db:"quantity"`
+	CheckoutID string `db:"checkout_id"`
+}
+
 type ProductCheckoutRequest struct {
-	ProductID string `json:"productId"`
-	Amount    int    `json:"amount"`
+	ProductID string `json:"productId" binding:"uuid4"`
+	Quantity  int    `json:"quantity" binding:"min=1"`
 }
 
 type CheckoutRequest struct {
-	CustomerID     string                   `json:"customerId"`
-	ProductDetails []ProductCheckoutRequest `json:"productDetails"`
-	Paid           int                      `json:"paid"`
-	Change         int                      `json:"change"`
+	CustomerID     string                   `json:"customerId" binding:"omitempty,uuid4"`
+	ProductDetails []ProductCheckoutRequest `json:"productDetails" binding:"required,min=1,dive"`
+	Paid           int                      `json:"paid" binding:"min=1"`
+	Change         int                      `json:"change" binding:"min=0"`
 }
 
 type CheckoutResponse struct {
@@ -32,24 +38,31 @@ type CheckoutResponse struct {
 	CreatedAt time.Time `json:"createdAt"`
 }
 
-func (cr *CheckoutRequest) NewCheckouts() []Checkout {
+func (cr *CheckoutRequest) NewCheckouts() (Checkout, []ProductCheckout) {
 	id := uuid.New()
 	rawCreatedAt := time.Now().Format(time.RFC3339)
 	createdAt, _ := time.Parse(time.RFC3339, rawCreatedAt)
 
-	var checkouts []Checkout
-	for _, v := range cr.ProductDetails {
-		checkout := Checkout{
-			ID:             id.String(),
-			CreatedAt:      createdAt,
-			UserCustomerID: cr.CustomerID,
-			ProductID:      v.ProductID,
-			Paid:           cr.Paid,
-			Change:         cr.Change,
-		}
-
-		checkouts = append(checkouts, checkout)
+	checkout := Checkout{
+		ID:             id.String(),
+		CreatedAt:      createdAt,
+		UserCustomerID: cr.CustomerID,
+		Paid:           cr.Paid,
+		Change:         cr.Change,
 	}
 
-	return checkouts
+	var productCheckouts []ProductCheckout
+	for _, v := range cr.ProductDetails {
+		id := uuid.New()
+		productCheckout := ProductCheckout{
+			ID:         id.String(),
+			ProductID:  v.ProductID,
+			Quantity:   v.Quantity,
+			CheckoutID: checkout.ID,
+		}
+
+		productCheckouts = append(productCheckouts, productCheckout)
+	}
+
+	return checkout, productCheckouts
 }
--- migrations/20240506072202_create_checkouts_table.up.sql
+++ migrations/20240506072202_create_checkouts_table.up.sql
@@ -1,12 +1,9 @@
 CREATE TABLE IF NOT EXISTS checkouts (
   id uuid PRIMARY KEY,
   user_customer_id uuid NOT NULL,
-  product_id uuid NOT NULL,
   paid int NOT NULL,
   change int NOT NULL,
   created_at timestamptz
 );
 
 ALTER TABLE checkouts ADD CONSTRAINT fk_user_customer_id_checkouts FOREIGN KEY (user_customer_id) REFERENCES user_customers (id);
-
-ALTER TABLE checkouts ADD CONSTRAINT fk_product_id_checkouts FOREIGN KEY (product_id) REFERENCES products (id);
