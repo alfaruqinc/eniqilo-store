e0d2bf4bfb617f8ca46f256081abfc6a6465fe24
umar006 <umaralfaruq597@gmail.com>
2024-05-08 10:02:55 +0700

    feat: get customers repository

--- internal/repository/user-customer.go
+++ internal/repository/user-customer.go
@@ -8,6 +8,7 @@ import (
 
 type UserCustomerRepository interface {
 	CreateUserCustomer(ctx context.Context, db *sql.DB, userCustomer domain.UserCustomer) error
+	GetCustomers(ctx context.Context, db *sql.DB, queryParams string, args []any) ([]domain.UserCustomerResponse, error)
 }
 
 type userCustomerRepository struct{}
@@ -27,4 +28,32 @@ func (ucr *userCustomerRepository) CreateUserCustomer(ctx context.Context, db *s
 	}
 
 	return nil
+}
+
+func (ucr *userCustomerRepository) GetCustomers(ctx context.Context, db *sql.DB, queryParams string, args []any) ([]domain.UserCustomerResponse, error) {
+	query := `
+		SELECT id, phone_number, name
+		FROM user_customers
+	`
+	query += queryParams
+
+	rows, err := db.QueryContext(ctx, query, args...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var customers []domain.UserCustomerResponse
+	for rows.Next() {
+		customer := domain.UserCustomerResponse{}
+
+		err := rows.Scan(&customer.ID, &customer.PhoneNumber, &customer.Name)
+		if err != nil {
+			return nil, err
+		}
+
+		customers = append(customers, customer)
+	}
+
+	return customers, nil
 }
