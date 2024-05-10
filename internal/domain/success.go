316196f971fac989fb98582d3a507b5e043ce760
umar006 <umaralfaruq597@gmail.com>
2024-05-10 22:13:00 +0700

    feat: omitempty data response message

--- internal/domain/success.go
+++ internal/domain/success.go
@@ -2,7 +2,7 @@ package domain
 
 type SuccessData struct {
 	Message string `json:"message"`
-	Data    any    `json:"data"`
+	Data    any    `json:"data,omitempty"`
 }
 
 func NewMessageSuccess(msg string, data any) SuccessData {
